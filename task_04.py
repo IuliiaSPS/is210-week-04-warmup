a8de1566c101d94b0b8e1c07d13acb720d668b36
Iuliia Ambrokh <iuliia.ambrokh@spsmail.cuny.edu>
2016-09-30 17:19:10 +0000

    Task_04

--- task_04.py
+++ task_04.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 """Function with three parameters."""
 
-def too_many_kittens(kittens, litterboxes, catfood):
-    """This function counts litterboxes.
 
+def too_many_kittens(kittens, litterboxes, catfood):
+
+    """This function counts litterboxes.
         Arg:
             kittens(int) = Just a random number.
             litterboxes (int) = Just a random number.
@@ -15,12 +16,11 @@ def too_many_kittens(kittens, litterboxes, catfood):
             Bool: True if there is enough litterboxes and catfood for kittens.
             False if there is not enough litterboxes and catfood for kittens.
 
-        Example:
+        Examples:
 
-            >>>too_many_kittens(12, 12, False)
+            >>> too_many_kittens(12, 12, False)
             True
-        
-            >>>too_many_kittens(13, 12, True)
+            >>> too_many_kittens(13, 12, True)
             True
      """
     return not (litterboxes >= kittens and catfood)
